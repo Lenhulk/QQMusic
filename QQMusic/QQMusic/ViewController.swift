f538c5e1e15912d0d1c5df62c948bb6583917ede
Lenhulk <Lenhulk@yeah.net>
2016-11-28 15:22:23 +0800

    初始化

---
+++ QQMusic/QQMusic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QQMusic
+//
+//  Created by Lenhulk on 2016/11/28.
+//  Copyright © 2016年 Lenhulk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
